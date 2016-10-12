391ab5b45727fef3d1eb27ed5287e4931ac56551
Kevin Perez <unknotted26@gmail.com>
2016-10-12 01:20:18 -0400

    Fixed StringBuilder declaration. First constructor argument must be in double quotes else the single quotes will can the (int,int) constructor.

--- PostMachine/Program.cs
+++ PostMachine/Program.cs
@@ -35,10 +35,15 @@ namespace PostMachine
     class PostMachine
     {
         private static int maxIterations = 10000;
-        private List<string> program_; // stores programs
-        private Queue<char> tape_;     // the tape
-        private char state_;           // post machine's state 
+        private List<string> program_; // stores programs (instructions)
+        private Queue<char> tape_;     // the tape input strings
+        private char internalState_;           // post machine's state 
 
+        private enum States
+        {
+            Start = 'S',
+            Halt = 'H'
+        };
         public PostMachine()
         {
             Console.WriteLine("PostMachine initialized.");
@@ -47,34 +52,36 @@ namespace PostMachine
         }
 
         /// <summary>
-        /// Load the input for the post machine into List<string> program_
+        /// Load the input for the post machine from a file (.pp - "post program") into List<string> program_.
+        /// The file represents instructions (one per line) 
         /// </summary>
         /// <param name="batch"></param>
-        /// <returns></returns>
+        /// <returns> bool </returns>
         public bool Load(bool batch = false)
         {
-            StreamReader in1; 
-            string filename;
+            StreamReader fstreamReader; 
+            
+            // Open input file 
             do
             {
                 Console.Write(" Name of instruction file: ");
-                filename = Console.ReadLine();
-                if (!OpenText(out in1, filename))
+                string filename = Console.ReadLine();
+                if (!OpenText(out fstreamReader, filename))
                     Console.WriteLine("** Cannot open file'" + filename + "'. Try again");
-            } while (in1 == StreamReader.Null);
+            } while (fstreamReader == StreamReader.Null);
             
             // if (batch) Console.WriteLine(filename);
 
             program_.Clear();
             string s;
-            int count = 0;
-            while ( (s = in1.ReadLine()) != null ) 
+            
+            while ( (s = fstreamReader.ReadLine()) != null ) 
             {
                 int pos = s.IndexOf('*', 0); // returns -1 if value is not found
-                if (pos == -1) pos = s.Length; 
-                if (pos < s.Length - 1 && pos > 0)
+                if (pos < 0) pos = s.Length; 
+                if (pos > 0)// (pos < s.Length - 1 && pos > 0) //  not a comment (> 0) 
                 {
-                    StringBuilder temp = new StringBuilder(' ', pos);
+                    StringBuilder temp = new StringBuilder(" ", pos); 
                     for (int j = 0; j < pos; ++j)
                         temp[j] = s[j];
                     Console.WriteLine(s);
@@ -82,73 +89,78 @@ namespace PostMachine
                 }
                 else if (pos == 0 && s.IndexOf('\n', 0) == 1)  program_.Add(s);
                 else if (pos == s.Length)  program_.Add(s);
-                
             }
-            in1.Dispose();
+            fstreamReader.Dispose();
             return true;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="batch"></param>
         public void Run(bool batch = false)
         {
-            char ch;
-            bool finished, halt, crash, maxitersReached, match;
-            long its = 0;
+            
+            bool finished = false, halt = false, crash = false, maxitersReached = false, match = false;
+            internalState_ = 'S';
 
             while (true)
             {
                 tape_.Clear();
                 Console.Write("  Input string (* to end): ");
                 // if(its == 0)
-                ch = (char) Console.Read(); // Console.ReadKey().KeyChar;
+                char input = (char) Console.Read(); // Console.ReadKey().KeyChar;
                 // if(batch) put
-                if (ch == '*')
+                if (input == '*') // end of program OR if at the start of a line, a comment.
                 {   
                     // Console.WriteLine("{0}", Console.OpenStandardInput());
                     // Utility.ClearBuffer(Console.OpenStandardInput());  //Console.OpenStandardInput());
                     // if(batch) Console.Write('\n');
                 }
-                while (ch != '\n')
+                while (input != '\n')
                 {
-                    tape_.Enqueue(ch);
-                    ch = (char) Console.Read(); // Console.ReadKey().KeyChar;
+                    tape_.Enqueue(input);
+                    input = (char) Console.Read(); // Console.ReadKey().KeyChar;
                     // if(batch) Console.Write('\n');
                 }
                 // if(batch) Console.Write('\n');
-                tape_.Enqueue('#');
+                tape_.Enqueue('#'); // # separates input strings
 
                 // Run post machine
-                state_ = 'S';
-                finished = halt = crash = maxitersReached = false;
-                its = 0;
+                long iteration = 0;
                 do
                 {
                     match = false;
-                    foreach (var i in program_)
+                    foreach (var instruction in program_)
                     {
-                        string s = i;
-                        if (s.ElementAt(0).Equals(state_))
+                        char currentState = instruction.ElementAt(0);
+                        char ch = instruction.ElementAt(1);
+                        char newState = instruction.ElementAt(2);
+                        if (currentState.Equals(internalState_) && tape_.Count != 0 && ch == tape_.First())
                         {
-                            if (tape_.Count != 0)
-                            {
-                                char tapeNext = tape_.First();
-                                if (s.ElementAt(1) == tapeNext)
-                                {
+                            Console.WriteLine("program_ first char of instruction matches first tape_ element: {0}", tape_.First());
+                            //if (tape_.Count != 0)
+                            //{
+                                //char tapeNext = tape_.First();
+                                //if (instruction.ElementAt(1) == tape_.First())//tapeNext)
+                                //{
                                     match = true;
                                     tape_.Dequeue();
-                                    state_ = s.ElementAt(2);
-                                    for (int j = 3; j < s.Length; ++j)
+                                    internalState_ = instruction.ElementAt(2); // new state
+                                    for (int j = 3; j < instruction.Length; ++j) // 
                                     {
-                                        tape_.Enqueue(s.ElementAt(j));
+                                        tape_.Enqueue(instruction.ElementAt(j));
                                     }
-                                }
-                            }
+                                //}
+                            //}
                         }
                     } // foreach
 
-                    ++its;
-                    halt = (state_ == 'H');
+                    internalState_ = 'H';
+                    ++iteration;
+                    halt = (internalState_ == 'H');
                     crash = !match;
-                    maxitersReached = (its == maxIterations);
+                    maxitersReached = (iteration == maxIterations);
                     finished = halt || crash || maxitersReached;
 
                 } while (!finished);
@@ -165,7 +177,7 @@ namespace PostMachine
                 else if (crash)
                 {
                     Console.WriteLine("String rejected.");
-                    Console.WriteLine("Last state: " + state_);
+                    Console.WriteLine("Last state: " + internalState_);
                     Console.WriteLine("Tape contents at rejection: ");
                     foreach (var t in tape_)
                         Console.Write("{0} ", t);
--- PostMachine/README.md
+++ PostMachine/README.md
@@ -1,1 +1,5 @@
-﻿
+﻿The theoretical post machine, thought up by American mathematician Emil Post, can compute anything a modern computer can.
+It consists of 4 parts: Program memory, processor, processing "tape", and a processor. The processing tape contains one
+instruction per line in the form <state><char><new state><string> where the states are a single character and a string can
+contain 0 or more alpha characters.
+
